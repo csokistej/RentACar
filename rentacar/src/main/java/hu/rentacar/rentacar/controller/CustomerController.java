9dde705359011e163f66ba431d559fa49da764cb
kgergely <kalgergely@gmail.com>
2017-11-11 14:58:26 +0100

    Add login and register endpoint

--- rentacar/src/main/java/hu/rentacar/rentacar/controller/CustomerController.java
+++ rentacar/src/main/java/hu/rentacar/rentacar/controller/CustomerController.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hu.rentacar.rentacar.controller;
+
+import hu.rentacar.rentacar.model.Customer;
+import hu.rentacar.rentacar.service.CustomerService;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author szotyi
+ * @author kgergely
  */
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
     
+    @Autowired
+    CustomerService service;
+    
+    @RequestMapping("/{name}")
+    public String pathParameter(@PathVariable String name) {
+        return "Hello " + name; 
+    }
+    
+    @PostMapping("/login")
+    public Customer loginCustomer(@RequestBody Customer customer) throws NotFoundException {
+        return service.login(customer);
+    }
+    
+    @PostMapping("/register")
+    public Customer registerCustomer(@RequestBody Customer customer){
+        return service.register(customer);
+    }
 }
