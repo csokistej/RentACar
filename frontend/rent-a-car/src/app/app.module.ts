e40219298ad5eb33bc21408a2072323b8611b57a
kgergely <kalgergely@gmail.com>
2017-12-02 10:08:07 +0100

    Generated CarService component

--- frontend/rent-a-car/src/app/app.module.ts
+++ frontend/rent-a-car/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CustomerServiceComponent } from './customer-service/customer-service.component';
+import { CarServiceComponent } from './car-service/car-service.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerServiceComponent
+    CustomerServiceComponent,
+    CarServiceComponent
   ],
   imports: [
     BrowserModule
