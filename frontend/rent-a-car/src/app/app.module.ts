740f5ff67608c2c604f35c0940c4af13c11162ea
kgergely <kalgergely@gmail.com>
2017-12-02 10:10:00 +0100

    Generated RentService component

--- frontend/rent-a-car/src/app/app.module.ts
+++ frontend/rent-a-car/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CustomerServiceComponent } from './customer-service/customer-service.component';
 import { CarServiceComponent } from './car-service/car-service.component';
+import { RentServiceComponent } from './rent-service/rent-service.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerServiceComponent,
-    CarServiceComponent
+    CarServiceComponent,
+    RentServiceComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/rent-a-car/src/app/rent-service/rent-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rent-service',
+  templateUrl: './rent-service.component.html',
+  styleUrls: ['./rent-service.component.css']
+})
+export class RentServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
